1d7fce7f3be741ed2f587294217fe83bc575fbf3
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-07-17 11:53:36 +0200

    esto

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import SimpleForm from "./componets/functional/SimpleForm/simpleForm";
+import TextInput from "./componets/InputForm/TextInput";
 
 // import
 
@@ -8,5 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <SimpleForm></SimpleForm>
+    <TextInput></TextInput>
   </React.StrictMode>
 );
